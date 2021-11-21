2ab7838e1cd7884dc1c01d1f5d4fc8b3c64dee1a
dennisphan <denis.phan262@gmail.com>
2021-11-20 19:34:35 -0700

    Dlv 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,4 +9,10 @@ mpg_data <- read.csv(file='Resources/MechaCar_mpg.csv', check.names=F, stringsAs
 lmResult = lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data =mpg_data)
 summary(lmResult)
 
+coil_Data <- read.csv('Resources/Suspension_Coil.csv')
 
+total_summary <- coil_Data %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), Standard_dev=sd(PSI))
+total_summary
+
+lot_summary <- coil_Data %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), Standard_dev=sd(PSI), .groups='keep')
+lot_summary
